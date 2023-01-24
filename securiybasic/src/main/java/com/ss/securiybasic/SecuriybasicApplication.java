d89361b9b7ba597aaf0c464ece5c2059a46dd735
Shahzaib Ibraim <shahzaibibrahim96@gmail.com>
2023-01-24 19:19:55 +0500

    Initial commit

---
+++ securiybasic/src/main/java/com/ss/securiybasic/SecuriybasicApplication.java
@@ -0,0 +1,13 @@
+package com.ss.securiybasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuriybasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuriybasicApplication.class, args);
+	}
+
+}
---
+++ securiybasic/src/test/java/com/ss/securiybasic/SecuriybasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.securiybasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuriybasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
